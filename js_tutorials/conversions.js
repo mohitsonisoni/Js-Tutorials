35a1207a1d68d9d24b346bd09bd16932d37c2f4a
Mohit Soni <50561629+mohitsonisoni@users.noreply.github.com>
2023-09-19 12:32:23 +0000

    type conversion

---
+++ js_tutorials/conversions.js
@@ -0,0 +1,10 @@
+let firstNumber = 'aasfa';
+let converted = Number(firstNumber);
+console.log(converted);
+console.log(typeof converted);
+
+
+let firstBoolean = "";
+let BooleanConverted = Boolean(firstBoolean);
+// console.log(BooleanConverted);
+// console.log(typeof BooleanConverted);
